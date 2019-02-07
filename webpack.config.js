91b87b0f2bfe0540c8415b80b8d7cf9a2e549a9f
Nat Grant <email.natgrant@gmail.com>
2019-02-08 09:29:29 +1300

    adding SCSS to boilerplate

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,31 @@
-const path = require('path')
+const path = require("path");
 
 module.exports = {
-  entry: './client/index.js',
+  entry: "./client/index.js",
   output: {
-    path: path.join(__dirname, 'server/public'),
-    filename: 'bundle.js'
+    path: path.join(__dirname, "server/public"),
+    filename: "bundle.js"
   },
-  mode: 'development',
+  mode: "development",
   module: {
-    rules: [{
-      test: /\.jsx?$/,
-      loader: 'babel-loader',
-      exclude: /node_modules/
-    }]
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+        use: [
+          "style-loader", // creates style nodes from JS strings
+          "css-loader", // translates CSS into CommonJS
+          "sass-loader" // compiles Sass to CSS, using Node Sass by default
+        ]
+      }
+    ]
   },
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: [".js", ".jsx"]
   },
-  devtool: 'source-map',
+  devtool: "source-map",
   devServer: {
-    contentBase: './server/public'
+    contentBase: "./server/public"
   }
-}
+};
