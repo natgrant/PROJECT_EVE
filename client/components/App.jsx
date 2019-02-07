cfc20b675e966ca8859cd37938e739db178fd05d
Karl Brocklehurst <kairakau@gmail.com>
2019-02-08 12:17:23 +1300

    merge nat and api into master

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,15 +2,20 @@ import React from 'react'
 import { HashRouter as Router, Route } from 'react-router-dom'
 
 import SubmitForm from './Form'
+import Home from "./Home";
+
 
 const App = () => {
   return (
-    <Router>
-      <div>
-        <h1>React development has begun!</h1>
-        <Route exact path='/form' component={SubmitForm} />
-      </div>
-    </Router>
+    <React.Fragment>
+      <Home />
+      <Router>
+        <div>
+          <Route exact path='/form' component={SubmitForm} />
+        </div>
+      </Router>
+    </React.Fragment>
+
   )
 }
 
