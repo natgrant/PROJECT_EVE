bca69dc2a0963a9440711c6a76ed6e0e4919d5f7
Nat Grant <email.natgrant@gmail.com>
2019-02-08 11:39:35 +1300

    changes to button

--- client/components/Button.jsx
+++ client/components/Button.jsx
@@ -2,8 +2,6 @@ import React from "react";
 
 export default function Button(props) {
   return (
-    <button className="enter" onClick={props.buttonClick}>
-      Enter If You Dare! {props.name}
-    </button>
+    <button onClick={props.buttonClick}>Enter If You Dare {props.name}</button>
   );
 }
--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,22 +1,29 @@
 import React, { Fragment, Component } from "react";
+import React from "react";
+import { Redirect } from "react-router-dom";
 import Button from "./Button";
 
 class Home extends Component {
   state = {
-    button: true
+    redirect: false
   };
-  click = () => {
+  setRedirect = () => {
     this.setState({
-      button: false
+      redirect: true
     });
+  };
+  renderRedirect = () => {
+    if (this.state.redirect) {
+      return <Redirect to="https://www.google.com" />;
+    }
   };
   render() {
     return (
       <Fragment>
         <div className="container">
           <h1>PROJECT EVE</h1>
-          {this.state.button == true && (
-            <Button name={this.state.button} buttonClick={this.click} />
+          {this.renderRedirect()}
+          <Button name={this.state.redirect} buttonClick={this.setRedirect} />
           )}
         </div>
       </Fragment>
