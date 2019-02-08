fac90bfdb6fd1a75cb02799b93c5f307f0619c83
Nat Grant <email.natgrant@gmail.com>
2019-02-08 14:12:18 +1300

    small style changes

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,25 +1,23 @@
-import React from 'react'
-import { HashRouter as Router, Route } from 'react-router-dom'
+import React from "react";
+import { HashRouter as Router, Route } from "react-router-dom";
 
-import SubmitForm from './Form'
+import SubmitForm from "./Form";
 import Home from "./Home";
-
 
 const App = () => {
   return (
     <React.Fragment>
-      <div className='container'>
+      <div className="container">
         <h1>PROJECT EVE</h1>
       </div>
       <Router>
         <div>
-          <Route exact path='/' component={Home} />
-          <Route path='/form' component={SubmitForm} />
+          <Route exact path="/" component={Home} />
+          <Route path="/form" component={SubmitForm} />
         </div>
       </Router>
     </React.Fragment>
+  );
+};
 
-  )
-}
-
-export default App
+export default App;
