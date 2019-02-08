1db7abfaae235b802870b8cf488b77da0348e34f
Alex <kuan8@hotmail.com>
2019-02-08 15:26:02 +1300

    fixed typo

--- server/server.js
+++ server/server.js
@@ -10,10 +10,12 @@ server.use(express.static(path.join(__dirname, "./public")));
 server.get("/api/haveibeenpwned/:email", (req, res) => {
   const apiEndPoint = "https://haveibeenpwned.com/api/v2/breachedaccount/";
   request
-    .get(apiEndPoint + req.param.email)
+    .get(apiEndPoint + req.params.email)
     .set("User-Agent", "Firefox")
     .then(apiRes => {
+      console.log(apiRes)
       res.json(apiRes.body);
+
     });
 });
 
